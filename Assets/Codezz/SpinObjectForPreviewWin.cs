a73c970b21ae2fd5ddb95e3feee05575fd37cb80
Baste Nesse Buanes <baste@rain-games.com>
2021-01-30 20:50:31 +0100

    Organizing, deleting, etc.

--- Assets/Codezz/SpinObjectForPreviewWin.cs
+++ Assets/Codezz/SpinObjectForPreviewWin.cs
@@ -1,11 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Temp : MonoBehaviour
-{
-    void Update()
-    {
+public class SpinObjectForPreviewWin : MonoBehaviour {
+    void Update() {
         transform.Rotate(Vector3.up, Time.deltaTime * 90f);
     }
 }
